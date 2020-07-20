15e88c5b7f443c3c18ad3d0ad03d2aa08c5b4678
0918nobita <nobita.0918@gmail.com>
2020-07-20 21:31:08 +0900

    :sparkles: 行検索機能を実装し、テストを追加

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use std::error::Error;
 use std::fs;
 
 pub struct Config<'a> {
-    pub query: &'a str,
-    pub filename: &'a str,
+    query: &'a str,
+    filename: &'a str,
 }
 
 impl<'a> Config<'a> {
@@ -20,6 +20,32 @@ impl<'a> Config<'a> {
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line)
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::search;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ fn main() {
         println!("Problem parsing arguments: {}", msg);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(&config) {
         println!("Application error: {}", e);
